1b289ce013cd3853c6900667930d61a1ac56ee9a
aka the owner <102393719+InvestorDoge@users.noreply.github.com>
2023-01-11 18:16:02 -0800

    Update e.js

--- e.js
+++ e.js
@@ -1,1 +1,9 @@
-print("Hello child")
+var password="4040";
+var guess;
+var guess=prompt('Enter Password','');
+if(password==guess)
+alert('Access Granted. Welcome to Doge Club!');
+else
+{
+  window.location="http://kicked.proxifiedbrowser.repl.co/";
+}
