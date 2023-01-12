b9c7152e6e78a3f211efd8f63d778d838c60ed53
aka the owner <102393719+InvestorDoge@users.noreply.github.com>
2023-01-11 18:55:48 -0800

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-var password="4040";
-var guess;
-var guess=prompt('Enter Password','');
-if(password==guess)
-alert('Access Granted. Welcome to Doge Club!');
-else
-{
-  window.location="http://kicked.proxifiedbrowser.repl.co/";
-}
